9eae783a537a7e589e375d5b58169d399d4e5bc1
GroundbreakingMC <noslowdwn@gmail.com>
2025-02-23 20:41:13 +0100

    Added UtilityClass annotation

--- src/main/java/com/github/groundbreakingmc/gigachat/collections/SocialSpyCollection.java
+++ src/main/java/com/github/groundbreakingmc/gigachat/collections/SocialSpyCollection.java
@@ -1,33 +1,31 @@
 package com.github.groundbreakingmc.gigachat.collections;
 
 import it.unimi.dsi.fastutil.objects.ObjectOpenHashSet;
+import lombok.experimental.UtilityClass;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 import java.util.Set;
 import java.util.UUID;
 
+@UtilityClass
 public final class SocialSpyCollection {
 
-    private static final Set<UUID> listening = new ObjectOpenHashSet<>();
+    private final Set<UUID> listening = new ObjectOpenHashSet<>();
 
-    private SocialSpyCollection() {
-
-    }
-
-    public static boolean contains(final UUID uuid) {
+    public boolean contains(final UUID uuid) {
         return listening.contains(uuid);
     }
 
-    public static void add(final UUID uuid) {
+    public void add(final UUID uuid) {
         listening.add(uuid);
     }
 
-    public static void remove(final UUID uuid) {
+    public void remove(final UUID uuid) {
         listening.remove(uuid);
     }
 
-    public static void sendAll(final Player sender, final Player recipient, final String message) {
+    public void sendAll(final Player sender, final Player recipient, final String message) {
         for (final UUID uuid : listening) {
             final Player player = Bukkit.getPlayer(uuid);
             if (player != null && player != sender && player != recipient) {
